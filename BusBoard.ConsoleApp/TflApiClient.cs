324adf71e8330475d313b6fc5897fb076dbedd37
Mike Walker <michael.walker@softwire.com>
2020-02-09 21:34:17 +0000

    Add AppId and AppKey to API Requests.

--- BusBoard.ConsoleApp/TflApiClient.cs
+++ BusBoard.ConsoleApp/TflApiClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using RestSharp;
 
 namespace BusBoard
@@ -6,15 +7,21 @@ namespace BusBoard
     public class TflApiClient
     {
         private readonly RestClient _restClient;
+        private readonly string _appId;
+        private readonly string _appKey;
         
         public TflApiClient()
         {
             _restClient = new RestClient("https://api.tfl.gov.uk");
+            _appId = Environment.GetEnvironmentVariable("TFL_APP_ID");
+            _appKey = Environment.GetEnvironmentVariable("TFL_APP_KEY");
         }
 
         public IEnumerable<ArrivalPrediction> FetchArrivalPredictionsForStopPoint(string stopPointId)
         {
-            var request = new RestRequest($"StopPoint/{stopPointId}/Arrivals");
+            var request = new RestRequest($"StopPoint/{stopPointId}/Arrivals")
+                .AddQueryParameter("app_id", _appId)
+                .AddQueryParameter("app_key", _appKey);
             var response = _restClient.Get<List<ArrivalPrediction>>(request);
             return response.Data;
         }
